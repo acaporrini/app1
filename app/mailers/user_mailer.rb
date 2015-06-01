7bd62f0126b8ae5b9414ae38cf2d0e07a3c11865
acaporrini <a.caporrini@gmail.com>
2015-06-01 17:10:03 +0200

    correct mailer method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: "a.caporrini@gmail.com"
 
-  def contact_form(email, name, message)
+  def contact_form(email, name, phone, message)
   @message = message
+  @email = email
+  @phone = phone
     mail(:from => email, 
         :to => 'a.caporrini@gmail.com', 
         :subject => "A new contact form message from #{name}")
