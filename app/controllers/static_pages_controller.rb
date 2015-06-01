9fe018082d33410375036a7c4a0650e8d8b63ea9
acaporrini <a.caporrini@gmail.com>
2015-06-01 17:07:21 +0200

    add mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,16 @@ class StaticPagesController < ApplicationController
   def index
     @products = Product.limit(3)
   end
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @phone = params[:phone]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @phone, @message).deliver
+
+  # ActionMailer::Base.mail(:from => @email, 
+  #     :to => 'a.caporrini@gmail.com', 
+  #     :subject => "A new contact form message from #{@name}", 
+  #     :body => (@message).deliver
+  # end
 end
