ebbe821144a473f6dc30b3e09cf0f27abcdb86e3
acaporrini <a.caporrini@gmail.com>
2015-06-07 12:10:32 +0200

    add validation for subscription form

--- app/assets/javascripts/my-js.js
+++ app/assets/javascripts/my-js.js
@@ -1,14 +1,19 @@
 
 $(document).ready(function(){
-  $(".toggleDesc").on("click",function(event){
-    event.preventDefault();
-    $(this).closest("section").find(".description").slideToggle();
-  })
+  // $(".toggleDesc").on("click",function(event){
+  //   event.preventDefault();
+  //   $(this).closest("section").find(".description").slideToggle();
+  // })
+
+   
   image();
   $("#image_url").on("change",function(){
     image();
+
   });
+
 });
+
   function image(){
     $(".image").html("<img class='img-thumbnail' src='"+$("#image_url").val()+"' alt='image not found'>");
   }
