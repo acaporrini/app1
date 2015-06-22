1898d226b3df4a777f44349cc29666e7a5926a62
acaporrini <a.caporrini@gmail.com>
2015-06-22 19:47:23 +0200

    add tests for Product and Comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
   scope :search, -> (search_term){ where("lower(name) LIKE ? OR lower(description) LIKE ?", "%#{search_term.downcase}%", "%#{search_term.downcase}%") }
 
   validates :name, presence: true
+  
   def average_rating
     comments.average(:rating).to_f
   end
