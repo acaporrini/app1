dde56483d82aab7d6e13146e57699c36cd512747
acaporrini <a.caporrini@gmail.com>
2015-06-25 20:34:44 +0200

    add functional test for static_pages and product controllers

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -7,10 +7,79 @@ describe ProductsController, type: :controller do
       @user.confirm
       sign_in @user
     end
-    it "create a new product" do
-      
-      expect{ post :create, { product: { name: "aaaa" } } }.to change{Product.count}.by(1)
-      
+    it "create a new product" do 
+      expect{ post :create, { product: { name: "aaaa" } } }.to change{Product.count}.by(1)      
     end
   end
+
+  describe "DELETE # products" do
+    before do
+      @user = User.create(email: "demo@demo.com", password: "12345678")
+      @user.confirm
+      sign_in @user
+      @product = Product.create(name: "bike",description: "cool bycicle",image_url: "bike.jpg",colour: "red",price: "2,99") 
+    end
+    it "delete an existing product" do    
+      expect{ delete :destroy, id: @product.id }.to change{Product.count}.by(-1)      
+    end
+  end
+  describe "UPDATE # products" do
+    before do
+      @user = User.create(email: "demo@demo.com", password: "12345678")
+      @user.confirm
+      sign_in @user
+      @product = Product.create(name: "bike",description: "cool bycicle",image_url: "bike.jpg",colour: "red",price: "2,99") 
+      put :update, id: @product.id, product: { name: "bycicle" }
+      @product.reload
+    end
+    it "update an existing product" do    
+      expect( @product.name ).to eq("bycicle")    
+    end
+  end
+  describe "GET #index" do
+    before do
+      @user = User.create(email: "demo@demo.com", password: "12345678")
+      @user.confirm
+      sign_in @user
+    end
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+  describe "GET #show" do
+    before do
+      @user = User.create(email: "demo@demo.com", password: "12345678")
+      @user.confirm
+      sign_in @user
+      @product = Product.create(name: "bike",description: "cool bycicle",image_url: "bike.jpg",colour: "red",price: "2,99") 
+    end
+    it "responds successfully with an HTTP 200 status code" do
+      get :show, id: @product.id
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :show, id: @product.id
+      expect(response).to render_template("show")
+    end
+  end
+  # describe "UPDATE # products" do
+  #   before do
+  #     @user = User.create(email: "demo@demo.com", password: "12345678")
+  #     @user.confirm
+  #     sign_in @user
+  #     @product = Product.create(name: "bike",description: "cool bycicle",image_url: "bike.jpg",colour: "red",price: "2,99") 
+  #   end
+  #   it "update an existing product" do    
+  #     expect{ put :update, id: @product.id, product: { name: "bycicle" }  }.to change{@product.name}     
+  #   end
+  # end
 end
---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe StaticPagesController, :type => :controller do
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+  describe "GET #about" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :about
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :about
+      expect(response).to render_template("about")
+    end
+  end
+  describe "GET #contact" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :contact
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :contact
+      expect(response).to render_template("contact")
+    end
+  end
+
+
+end
