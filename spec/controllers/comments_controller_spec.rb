7f912133ba46d67ce79f6359265258a0276fca54
acaporrini <a.caporrini@gmail.com>
2015-06-28 17:45:27 +0200

    add cancancan ability test for comment controller

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+require "cancan/matchers"
 describe CommentsController, type: :controller do
   describe "POST # comments" do
     before do
@@ -18,18 +18,49 @@ describe CommentsController, type: :controller do
   end
   describe "DELETE # comments" do
     before do
-      @user = User.create(email: "demo@demo.com", password: "12345678")
+      @user = User.create(email: "demo@demo.com", password: "12345678", admin: false)
       @user.confirm
       sign_in @user
       @product = Product.create(name: "bike",description: "cool bycicle",image_url: "bike.jpg",colour: "red",price: "2,99") 
       @comment = Comment.create(body:"comment's body", rating: 3, product_id: @product.id, user_id: @user.id )
+      @ability = Ability.new(@user)
     end
     it "delete a comment" do 
       expect{ delete :destroy,  product_id: @product.id, id: @comment.id }.to change{Comment.count}.by(-1)      
     end
+
     it "redirect to product" do
       delete :destroy,  product_id: @product.id, id: @comment.id
       expect(response).to redirect_to(@product)     
+    end
+
+  end
+  describe "cancancan ability" do
+    before do
+      @user = User.create(email: "demo@demo.com", password: "12345678", admin: false)
+      @user.confirm
+      sign_in @user
+      @product = Product.create(name: "bike",description: "cool bycicle",image_url: "bike.jpg",colour: "red",price: "2,99") 
+      @comment = Comment.create(body:"comment's body", rating: 3, product_id: @product.id, user_id: @user.id )
+
+    end
+    context "user is not admin" do
+      before do
+        @ability = Ability.new(@user)
+      end
+      it "should not have the ability to destroy" do
+        expect(@ability).to_not be_able_to(:destroy, @comment)
+      end
+    end
+    context "user is admin" do
+      before do
+        @user.admin = true
+        @ability = Ability.new(@user)
+      end
+      it "should have the ability to destroy" do
+        expect(@ability).to be_able_to(:destroy, @comment)
+      end
+
     end
 
   end
