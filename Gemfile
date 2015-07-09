7468e55b1384829f81995b9d902d05a078d09311
acaporrini <a.caporrini@gmail.com>
2015-07-09 22:43:56 +0200

    add brakeman

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.2.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.1'
+gem 'rails', '4.2.2'
 # Use sqlite3 as the database for Active Record
 gem 'puma'
 # makes Jquery works with turbolinks
@@ -42,6 +42,7 @@ gem 'pry-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'will_paginate', '~> 3.0.6'
+gem "brakeman", :require => false
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,10 +8,10 @@ class PaymentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @message = "Congratulations! You have just bought a #{@product.name} for #{ sprintf('%.2f', @amount / 100) } €"
 
-    #byebug
+    byebug
     begin
       charge = Stripe::Charge.create(
-        :amount => @amount.to_i, # amount in cents, again
+        :amount => @amount.to_i, 
         :currency => "eur",
         :source => token,
         :description => params[:stripeEmail]
@@ -19,7 +19,7 @@ class PaymentsController < ApplicationController
       flash[:notice] = "Thanks for your purchase, you paid #{ sprintf('%.2f', @amount / 100) } €"
       UserMailer.payment_notification(@email, @message).deliver
     rescue Stripe::CardError => e
-      # The card has been declined
+     
       body = e.json_body
       err  = body[:error]
       flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
