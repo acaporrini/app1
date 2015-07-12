8ad9862e15e55881f0ab8755006122194e64d7dd
acaporrini <a.caporrini@gmail.com>
2015-07-12 20:20:19 +0200

    add caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,8 +11,8 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-
+  config.action_controller.perform_caching = true
+  config.perform_caching = true
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.action_controller.perform_caching = true
   config.action_mailer.delivery_method = :letter_opener
-
+  config.cache_store = :dalli_store
 end
